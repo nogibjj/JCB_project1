abcbf0556fc5171d62bc4badbd2c6927db7f67ca
cervs257 <cervantes257@gmail.com>
2023-09-18 16:04:29 -0400

    wrote yf_currencies script

--- src/yf_currencies.py
+++ src/yf_currencies.py
@@ -1,1 +1,32 @@
-# a python script that imports lib.py and performs some descriptive statistics
+""" This is a script that performs some descriptive analysis on diverse currencies """
+
+# Let's import the descriptive statistics we've created in lib.py and a few methods from our download_currencies module:
+
+from lib import read_data, print_range, plot_returns
+from download_currencies import download_prices_long, create_tickers
+import polars as pl
+
+# use download_prices_long to download hourly data on usdmxn, eurusd, nzdusd for the last 6 months
+currencies = ["usdmxn", "eurusd", "nzdusd"]
+tickers = create_tickers(currencies)
+download_prices_long(tickers)
+
+# import data as polars dataframe
+prices = read_data("currency_prices_long.csv")
+print(prices.head())
+print(prices["Price type"].unique())
+
+# Let's look at a quick example of each currency's average price by Price type for the last 6 months:
+average_prices = (
+    prices.group_by(["Instrument", "Price type"])
+    .mean()
+    .filter(pl.col("Price type") != "Volume")[["Instrument", "Price type", "Price"]]
+    .sort(by="Instrument")
+)
+print(average_prices)
+
+# a more specific description of price ranges using Price type == Close
+print_range(prices)
+
+# plot each currency's cumulative returns over the period
+plot_returns(prices, "Close")
