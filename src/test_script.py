e216973a77a25e4796f93ec499ab9857f57f322c
cervs257 <cervantes257@gmail.com>
2023-09-18 17:04:07 -0400

    renamed test_yf_currencies to test_script

--- src/test_script.py
+++ src/test_script.py
@@ -1,1 +1,8 @@
-# test the script
+""" Test script for yf_currencies """
+from yf_currencies import *
+
+
+def test_yf_currencies():
+    assert "usdmxn" in currencies
+    assert prices.shape[1] == 5
+    assert "High" in average_prices["Price type"]
